fc9a75fb6671aa8900bc018efa1574258e33659c
Tano Rojas <lisandro@compucorp.co.uk>
2021-08-13 14:44:57 +0700

    CPS-601: Remove activity creation for prospects

--- gulp-tasks/utils/get-active-prospecting-id.js
+++ gulp-tasks/utils/get-active-prospecting-id.js
@@ -3,23 +3,34 @@ const cvApi = require('./cv-api.js');
 module.exports = getActiveProspectingId;
 
 /**
- * Returns the ID of a application that is active and has an activity for the current
- * calendar month.
+ * Returns the ID of one of the existent prospects.
  *
- * @returns {number} case id of an active case
+ * @returns {number} prospect id
  */
 function getActiveProspectingId () {
-  var activity = cvApi('Activity', 'get', {
+  const prospectTypeId = cvApi('CaseType', 'get', {
     sequential: 1,
-    'case_id.is_deleted': 0,
-    case_filter: { 'case_type_id.case_type_category': 'prospecting' },
-    return: ['case_id'],
+    is_active: 1,
+    case_type_category: 'Prospecting',
+    return: ['id'],
     options: { limit: 1 }
   });
 
-  if (!activity.count) {
-    throw new Error('Please add an activity for a prospecting');
+  if (!prospectTypeId.count) {
+    throw new Error('Please add a prospect type');
   }
 
-  return activity.count && activity.values[0].case_id[0];
+  const prospecting = cvApi('Case', 'get', {
+    sequential: 1,
+    is_deleted: 0,
+    case_type_id: prospectTypeId.values[0].id,
+    return: ['id'],
+    options: { limit: 1 }
+  });
+
+  if (!prospecting.count) {
+    throw new Error('Please add a prospecting');
+  }
+
+  return prospecting.values[0].id;
 }
