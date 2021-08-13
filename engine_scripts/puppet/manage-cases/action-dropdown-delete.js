208836b766d97a6b0a7e7ebc39f77ab5a6fe76f9
Tano Rojas <lisandro@compucorp.co.uk>
2021-08-13 14:22:03 +0700

    CPS-601: Use action-dropdown script on new actions

--- engine_scripts/puppet/manage-cases/action-dropdown-delete.js
+++ engine_scripts/puppet/manage-cases/action-dropdown-delete.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = async (page, scenario, vp) => {
-  await require('./action-dropdown')(page, scenario, vp);
+  await require('../action-dropdown')(page, scenario, vp);
   await page.evaluate(() => {
     CRM.$('.civicase__case-header__action-menu .btn-group:last-child .dropdown-menu li a:contains("Delete Case")').click();
   });
--- engine_scripts/puppet/manage-prospectings/click-contribution-button.js
+++ engine_scripts/puppet/manage-prospectings/click-contribution-button.js
@@ -4,11 +4,8 @@ const Utility = require('../utility.js');
 
 module.exports = async (page, scenario, vp) => {
   const utility = new Utility(page, scenario, vp);
-  await utility.waitForAngular();
-  await utility.waitForLoadingComplete();
 
-  await page.click('.civicase__case-header__action-icon.btn.btn-primary');
-
+  await require('../action-dropdown')(page, scenario, vp);
   await page.evaluate(() => {
     CRM.$('.civicase__case-header__action-menu .btn-group:last-child .dropdown-menu li a:contains("Convert to Contribution")').click();
   });
